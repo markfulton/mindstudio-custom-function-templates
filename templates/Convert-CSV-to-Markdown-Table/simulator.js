4bd728b70d3e6d3692c538428173b1fd9a0ab933
markfulton <markfulton.com@gmail.com>
2024-06-07 15:37:51 -0400

    Create simulator.js

---
+++ templates/Convert-CSV-to-Markdown-Table/simulator.js
@@ -0,0 +1,10 @@
+environment = {
+  config: {
+    csvResults: '{{csvResults}}',
+    outputVar: 'markdownOutput',
+  },
+  vars: {
+    csvResults: 'Name, Age, City\nJohn Doe, 29, New York\nJane Smith, 24, Los Angeles',
+    markdownOutput: '',
+  },
+}
